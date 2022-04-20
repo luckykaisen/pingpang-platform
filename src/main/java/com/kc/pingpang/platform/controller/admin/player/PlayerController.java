690cedf120309e9f6a5c526796ce1be9c9479af5
weikaisen <374362311@qq.com>
2022-04-20 17:14:41 +0800

    【魏凯森】保存

--- src/main/java/com/kc/pingpang/platform/controller/admin/player/PlayerController.java
+++ src/main/java/com/kc/pingpang/platform/controller/admin/player/PlayerController.java
@@ -9,10 +9,12 @@ import com.kc.pingpang.platform.service.player.api.IPlayerService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@RestController("AdminPlayerController")
 @RequestMapping("/services/rs/admin/player")
 public class PlayerController {
 
